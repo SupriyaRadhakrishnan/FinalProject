a3fc96cd876e45fd377d2f51640f0aad1f88326b
timstetter <tstetter@gmail.com>
2020-12-08 09:28:45 -0500

    More Formatting

--- src/main/java/co/grandcircus/YelpFusion/Controller/ModelController.java
+++ src/main/java/co/grandcircus/YelpFusion/Controller/ModelController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import co.grandcircus.YelpFusion.Model.*;
 import co.grandcircus.YelpFusion.Service.*;
 
-
 @Controller
 public class ModelController {
 
@@ -20,23 +19,23 @@ public class ModelController {
 
 	@Autowired
 	private UserRepository urep;
-	
+
 	@Autowired
 	private UserGroupRepository ugrep;
-	
+
 	@Autowired
 	private YelpFusionService yfs;
-	
+
 	@Autowired
 	private EventRepository erep;
-	
+
 	@Autowired
 	private ActivityRepository arep;
-	
+
 	@Autowired
 	private BusinessRepository brep;
-	
-	@PostMapping("/creategroup") 
+
+	@PostMapping("/creategroup")
 	public String creategroup(String email, String groupname, Model model) {
 		User user = urep.findByEmail((String) session.getAttribute("useremail"));
 
@@ -44,8 +43,8 @@ public class ModelController {
 		List<User> memberlist = new ArrayList<User>();
 		UserGroup ug = new UserGroup();
 		String memberinput[] = email.split(";");
-		
-		ug.setGroupname(groupname);	    
+
+		ug.setGroupname(groupname);
 
 		for (int i = 0; i < memberinput.length; i++) {
 			System.out.println(memberinput[0]);
@@ -65,128 +64,109 @@ public class ModelController {
 		urep.save(user);
 		model.addAttribute("groups", user.getUsergroup());
 		model.addAttribute("username", session.getAttribute("username"));
-		
-		
+
 		return "/index";
 
 	}
-	
-	
+
 	@GetMapping("/groupdetails/{groupid}")
-	public String groupdetails(@PathVariable("groupid") long id,Model model)
-	{
-		
+	public String groupdetails(@PathVariable("groupid") long id, Model model) {
+
 		UserGroup ug = ugrep.findById(id).orElse(null);
-		model.addAttribute("groupinfo",ug);
-		model.addAttribute("event",ug.getEvents());
+		model.addAttribute("groupinfo", ug);
+		model.addAttribute("event", ug.getEvents());
 		return "groupinfo";
 	}
-	
-	
-	
-	
-	@PostMapping("/createevent") 
-	public String createevent(String pricerange,@RequestParam(value = "groupid") long groupid,Event event,@RequestParam(value = "category") List<String> category, Model model) {
+
+	@PostMapping("/createevent")
+	public String createevent(String pricerange, @RequestParam(value = "groupid") long groupid, Event event,
+			@RequestParam(value = "category") List<String> category, Model model) {
 		User user = urep.findByEmail((String) session.getAttribute("useremail"));
 		List<Business> listofBusiness = new ArrayList<>();
 		List<Activity> activitylist = new ArrayList<>();
-		BusinessResponse bs= null;
+		BusinessResponse bs = null;
 		UserGroup groupinfo = ugrep.findById(groupid).get();
-		System.out.println("groupinfo"  + groupinfo);
+		System.out.println("groupinfo" + groupinfo);
 		event.setUsergroup(groupinfo);
 		event.setEventadmin(user.getId());
 
-		 erep.save(event);
-		 if(pricerange.equals("1"))
-		 {
-			 pricerange = "1";
-		 }
-		 else if(pricerange.equals("2"))
-		 {
-			 pricerange ="1,2";
-		 }
-		 else if(pricerange.equals("3"))
-		 {
-			 pricerange ="1,2,3";
-		 }
-		 else
-		 {
-			 pricerange ="1,2,3,4";
-		 }
-	
-	System.out.println("pricerange" + pricerange);
-		for(String c: category)
-		{
-			 bs = yfs.getBusinesses(event.getEventcity(), c,pricerange);
+		erep.save(event);
+		if (pricerange.equals("1")) {
+			pricerange = "1";
+		} else if (pricerange.equals("2")) {
+			pricerange = "1,2";
+		} else if (pricerange.equals("3")) {
+			pricerange = "1,2,3";
+		} else {
+			pricerange = "1,2,3,4";
+		}
+
+		System.out.println("pricerange" + pricerange);
+		for (String c : category) {
+			bs = yfs.getBusinesses(event.getEventcity(), c, pricerange);
 			Activity activity = new Activity();
-			 activity.setActivityname(c);	
-			 activity.setBusiness(bs.getBusinesses());
-			 activitylist.add(activity);
-			 activity.setEvent(event);
-			 arep.save(activity);
-			 for(Business business : bs.getBusinesses())
-			 {
-				 listofBusiness.add(business);
-				 business.setActivity(activity);
-	             brep.save(business);
-			 }
-					
-		}   		
-	    model.addAttribute("event",groupinfo.getEvents());
+			activity.setActivityname(c);
+			activity.setBusiness(bs.getBusinesses());
+			activitylist.add(activity);
+			activity.setEvent(event);
+			arep.save(activity);
+			for (Business business : bs.getBusinesses()) {
+				listofBusiness.add(business);
+				business.setActivity(activity);
+				brep.save(business);
+			}
+
+		}
+		model.addAttribute("event", groupinfo.getEvents());
 		return "groupinfo";
 	}
-	
-	
+
 	@GetMapping("/eventdetails")
-	public String eventdetails(@RequestParam(value = "event") long id,Model model)
-	{
+	public String eventdetails(@RequestParam(value = "event") long id, Model model) {
 		System.out.println("inside eventdetails");
 		Event e = erep.findById(id).orElse(null);
 		System.out.println("event " + e);
-				model.addAttribute("event",e);	
+		model.addAttribute("event", e);
 		return "eventdetails";
 	}
-	
-	@PostMapping("/savevotes")
-	public String savevotes(@RequestParam(value ="eventid") long eventid,@RequestParam(required = false) String restaurants_favorite,@RequestParam(required = false) String restaurants_notfavorite,@RequestParam(required = false) String parks_favorite,@RequestParam(required = false) String parks_notfavorite, Model model)
-	{
 
-Event event = erep.findById(eventid).get();
-        List<Activity> activitylist = event.getActivity();
-        String favbusinessname ="";
-        String nfavbusinessname ="";
-for(Activity activity : activitylist)
-{
-	String activityname = activity.getActivityname();
-	if(activityname.equals("restaurants")){
-		 favbusinessname = restaurants_favorite;
-		 nfavbusinessname = restaurants_notfavorite;
-	}
-	else if(activityname.equals("parks"))
-	{
-		favbusinessname = parks_favorite;
-		 nfavbusinessname = parks_notfavorite;
-	}
-	if((activityname+"_favorite") !=null || (activityname+"_notfavorite")!=null )
-	{
-	List<Business> businesslist = activity.getBusiness();
-	for(Business business : businesslist)
-	{
-	
-		if(business.getName().equals(favbusinessname) && (favbusinessname) !=null)
-		{
-			System.out.println("inside favs");
-		business.setFavourite(business.getFavourite()+1);
+	@PostMapping("/savevotes")
+	public String savevotes(@RequestParam(value = "eventid") long eventid,
+			@RequestParam(required = false) String restaurants_favorite,
+			@RequestParam(required = false) String restaurants_notfavorite,
+			@RequestParam(required = false) String parks_favorite,
+			@RequestParam(required = false) String parks_notfavorite, Model model) {
+
+		Event event = erep.findById(eventid).get();
+		List<Activity> activitylist = event.getActivity();
+		String favbusinessname = "";
+		String nfavbusinessname = "";
+		for (Activity activity : activitylist) {
+			String activityname = activity.getActivityname();
+			if (activityname.equals("restaurants")) {
+				favbusinessname = restaurants_favorite;
+				nfavbusinessname = restaurants_notfavorite;
+			} else if (activityname.equals("parks")) {
+				favbusinessname = parks_favorite;
+				nfavbusinessname = parks_notfavorite;
+			}
+			if ((activityname + "_favorite") != null || (activityname + "_notfavorite") != null) {
+				List<Business> businesslist = activity.getBusiness();
+				for (Business business : businesslist) {
+
+					if (business.getName().equals(favbusinessname) && (favbusinessname) != null) {
+						System.out.println("inside favs");
+						business.setFavourite(business.getFavourite() + 1);
+					}
+					if (business.getName().equals(nfavbusinessname) && (nfavbusinessname) != null) {
+						System.out.println("inside notfavs");
+						business.setNotfavourite(business.getNotfavourite() + 1);
+					}
+					brep.save(business);
+				}
+			}
 		}
-		if(business.getName().equals(nfavbusinessname) && (nfavbusinessname) !=null)
-		{
-			System.out.println("inside notfavs");
-		business.setNotfavourite(business.getNotfavourite()+1);
-		}
-		brep.save(business);
-	}
-	}
-}          model.addAttribute("event",event);	
+		model.addAttribute("event", event);
 		return "eventdetails";
 	}
 
--- src/main/java/co/grandcircus/YelpFusion/Service/YelpFusionService.java
+++ src/main/java/co/grandcircus/YelpFusion/Service/YelpFusionService.java
@@ -1,6 +1,5 @@
 package co.grandcircus.YelpFusion.Service;
 
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,23 +11,22 @@ import co.grandcircus.YelpFusion.Model.BusinessResponse;
 
 @Service
 public class YelpFusionService {
-	
+
 	private RestTemplate rt = new RestTemplate();
 
-	public BusinessResponse getBusinesses(String location,String categories,String pricerange)
-	{
+	public BusinessResponse getBusinesses(String location, String categories, String pricerange) {
 		System.out.println("Hello");
 		String url = "https://api.yelp.com/v3/businesses/search?&location={location}&radius=40000&price={pricerange}";
-		if(!categories.isEmpty())
-		{
+		if (!categories.isEmpty()) {
 			url += "&categories={categories}";
 		}
 		// Set up headers.
 		String apiKey = "LDxP8dFS6C8yX--cCtLbIK-RZmboBOoUzKPJKHpSDEnz0a3KgpDAqY4nR6f3w8VgheRenPvi6CXMPM5inJuzh2Mn6no3FwYSzDHwxR-cBjRhJ2Yn0cqgQMQQGL7GX3Yx";
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "Bearer " + apiKey);
-		BusinessResponse response = rt.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), BusinessResponse.class,location,pricerange,categories).getBody();
+		BusinessResponse response = rt.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), BusinessResponse.class,
+				location, pricerange, categories).getBody();
 		System.out.println("response " + response);
-	return response;
+		return response;
 	}
 }
