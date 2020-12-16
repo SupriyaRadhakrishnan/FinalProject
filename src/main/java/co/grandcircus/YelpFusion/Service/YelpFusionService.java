bfb1697c370a35ba82c5b915f1811febcd272762
timstetter <tstetter@gmail.com>
2020-12-16 13:44:14 -0500

    moved apikey call to env var

--- src/main/java/co/grandcircus/YelpFusion/Service/YelpFusionService.java
+++ src/main/java/co/grandcircus/YelpFusion/Service/YelpFusionService.java
@@ -1,6 +1,5 @@
 package co.grandcircus.YelpFusion.Service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,14 +11,13 @@ import co.grandcircus.YelpFusion.Model.BusinessResponse;
 
 @Service
 public class YelpFusionService {
-	
-	@Value("${apikey}")
-	String apikey;
+
+	String apikey = System.getenv("APIKEY");
 
 	private RestTemplate rt = new RestTemplate();
 
 	public BusinessResponse getBusinesses(String location, String categories, String pricerange) {
-		
+
 		String url = "https://api.yelp.com/v3/businesses/search?&location={location}&radius=40000&price={pricerange}";
 		if (!categories.isEmpty()) {
 			url += "&categories={categories}";
@@ -29,7 +27,7 @@ public class YelpFusionService {
 		headers.add("Authorization", "Bearer " + apikey);
 		BusinessResponse response = rt.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), BusinessResponse.class,
 				location, pricerange, categories).getBody();
-	//	System.out.println("response " + response);
+		// System.out.println("response " + response);
 		return response;
 	}
 }
